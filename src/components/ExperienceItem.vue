939f7219e45e9071248a9a9c865ee9a20a91c0a2
Max Hayward <max.hayward@gmail.com>
2024-02-28 14:34:51 +0000

    added timeline graphic for experience section

--- src/components/ExperienceItem.vue
+++ src/components/ExperienceItem.vue
@@ -5,11 +5,22 @@ const props = defineProps<IExperience>()
 </script>
 
 <template>
-  <div class="mb-4 fade-in bg-gray-900 rounded-lg p-2">
-    <h3 class="text-2xl mb-2">{{ props.role }}</h3>
-    <p class="text-xl">{{ props.company }}</p>
-    <img v-if="props.image" :src="props.image" class="my-2 h-16" />
-    <p class="text-lg">{{ props.dates }}</p>
-    <p class="mt-2">{{ props.description }}</p>
+    <div class="">
+      <div class="relative h-full pl-8 pb-4 sm:pl-20 group">
+        <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
+        <div
+          class="flex flex-col sm:flex-row items-start group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[3.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-white after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[3.5rem] after:-translate-x-1/2 after:translate-y-1.5"
+        >
+        </div>
+        <div class="fade-in rounded-lg">
+          <p class="text-3xl">{{ props.company }}</p>
+          <img v-if="props.image" :src="props.image" class="my-2 h-16" />
+          <p class="text-lg">{{ props.dates }}</p>
+          <p class="mt-2">{{ props.description }}</p>
+        </div>
+      </div>
+
+      <div class="relative pl-8 sm:pl-32 group">
+      </div>
   </div>
 </template>
